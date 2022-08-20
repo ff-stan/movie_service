7cb53a31eb1d6c7b5233e321b20529529ddcb15b
Amadeus <fangfengstan@outlook.com>
2022-08-20 17:49:29 +0800

    增加请求参数过滤器防止sql注入 暂时关闭文件上传功能 上传到服务器上测试是否成功 22-08-20

--- routes/movie.js
+++ routes/movie.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var movie = require('../models/movie');
 var comment=require('../models/comment');
+var filter = require('../utils/sanitize');
+// 过滤参数
+const sanitize = require('mongo-sanitize');
 
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
@@ -17,11 +20,14 @@ router.get('/list', function (req, res, next) {
 //获得下载地址并将更新+1
 //用户下载只返回下载地址
 router.post('/download', function (req, res, next) {
-    if (!req.body.movie_id) {
+    let reqs = filter(req.body, function (prams) {
+        return sanitize(prams);
+      });
+    if (!reqs.movie_id) {
         res.json({status: 1, message: "电影id传递失败"})
     }else{
-        movie.findById(req.body.movie_id, function (err, supportMovie) {
-            movie.update({_id: req.body.movie_id}, {movieNumDownload: supportMovie[0].movieNumDownload + 1}, function (err) {
+        movie.findById(reqs.movie_id, function (err, supportMovie) {
+            movie.update({_id: reqs.movie_id}, {movieNumDownload: supportMovie[0].movieNumDownload + 1}, function (err) {
                 if (err) {
                     res.json({status: 1, message: "下载失败", data: err})
                 }
@@ -34,8 +40,11 @@ router.post('/download', function (req, res, next) {
 
 //获取相关电影的详细信息
 router.post('/detail', function (req, res, next) {
-    if(req.body.id) {
-        movie.findById(req.body.id,function (err, getMovie) {
+    let reqs = filter(req.body, function (prams) {
+        return sanitize(prams);
+      });
+    if(reqs.id) {
+        movie.findById(reqs.id,function (err, getMovie) {
             res.json({status: 0, message: '获取成功', data: getMovie})
         })
     }else{
@@ -45,8 +54,11 @@ router.post('/detail', function (req, res, next) {
 
 //获取相关电影的评论
 router.post('/comment', function (req, res, next) {
-    if(req.body.id) {
-        comment.findByMovieId(req.body.id, function (err, getComment) {
+    let reqs = filter(req.body, function (prams) {
+        return sanitize(prams);
+      });
+    if(reqs.id) {
+        comment.findByMovieId(reqs.id, function (err, getComment) {
             res.json({status: 0, message: '获取成功', data:getComment})
         })
 
@@ -57,8 +69,11 @@ router.post('/comment', function (req, res, next) {
 
 //获取相关电影的点赞和下载数(更改后)
 router.post('/showNumber', function (req, res, next) {
-    if(req.body.id) {
-        movie.findById(req.body.id,function (err, getMovie) {
+    let reqs = filter(req.body, function (prams) {
+        return sanitize(prams);
+      });
+    if(reqs.id) {
+        movie.findById(reqs.id,function (err, getMovie) {
             res.json({status: 0, message: '获取成功', data: {movieNumDownload: getMovie[0].movieNumDownload,movieNumSuppose:getMovie[0].movieNumSuppose}})
             // res.json({status: 0, message: '获取成功', data: getMovie[0]})
         })
@@ -69,7 +84,10 @@ router.post('/showNumber', function (req, res, next) {
 
 //获取主页电影推荐
 router.post('/getIndexMovie', function (req, res, next) {
-    if(req.body.id) {
+    let reqs = filter(req.body, function (prams) {
+        return sanitize(prams);
+      });
+    if(reqs.id) {
         movie.find({movieMainPage:true}, function (err, allMovie) {
             res.json({status: 0, message: '获取成功', data:allMovie})
         })
@@ -80,8 +98,11 @@ router.post('/getIndexMovie', function (req, res, next) {
 
 //获取一个电影的评论
 router.post('/getMovieComment', function (req, res, next) {
-    if(req.body.id) {
-        comment.findByMovieId(req.body.id, function (err, allComment) {
+    let reqs = filter(req.body, function (prams) {
+        return sanitize(prams);
+      });
+    if(reqs.id) {
+        comment.findByMovieId(reqs.id, function (err, allComment) {
             res.json({status: 0, message: '获取成功', data:allComment})
         })
     }else{
@@ -93,9 +114,12 @@ router.post('/getMovieComment', function (req, res, next) {
 //点赞的电影
 //后续需要限制次数
 router.post('/support', function (req, res, next) {
-    if(req.body.id) {
-        movie.findById(req.body.id, function (err, getMovie) {
-            movie.update({_id: req.body.id}, {movieNumSuppose: getMovie[0].movieNumSuppose+1}, function (err, movieUpdate) {
+    let reqs = filter(req.body, function (prams) {
+        return sanitize(prams);
+      });
+    if(reqs.id) {
+        movie.findById(reqs.id, function (err, getMovie) {
+            movie.update({_id: reqs.id}, {movieNumSuppose: getMovie[0].movieNumSuppose+1}, function (err, movieUpdate) {
                 if (err) {
                     res.json({status: 1, message: "点赞错误", data: err })
                 }
