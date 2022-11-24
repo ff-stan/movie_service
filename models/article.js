f7befdb3e06abf9e88544eec12c95e80f703a3b2
ff-stan <fangfengstan@outlook.com>
2022-11-24 12:27:51 +0800

    修改了一些细节 后续重新构造一下路由 使之符合RESTful API接口规格

--- models/article.js
+++ models/article.js
@@ -2,22 +2,22 @@
 var mongoose = require('../common/db');
 //数据库的数据集
 var article = new mongoose.Schema({
-    articleTitle:String,
-    articleContext:String,
-    articleTime:String,
+    articleTitle: String,
+    articleContext: String,
+    articleTime: { type: Date, default: Date.now },
     articleNumSuppose: Number,
-    articleAuthor:String
+    articleAuthor: String
 });
 //数据操作的一些常用方法
 //通过ID查找
-article.statics.findByArticleId = function(id,callBack){
-    this.find({_id:id},callBack);
+article.statics.findByArticleId = function (id, callBack) {
+    this.find({ _id: id }, callBack);
 };
 
 //找到所有的文章
-article.statics.findAll = function(callBack){
-    this.find({},callBack);
+article.statics.findAll = function (callBack) {
+    this.find({}, callBack);
 };
-var articleModel = mongoose.model('article',article);
+var articleModel = mongoose.model('article', article);
 
 module.exports = articleModel;
